a25e6cdb9e8fbdf4491124ec04f4bfe8cfafee95
birkin <birkin.diana@gmail.com>
2016-11-14 12:24:29 -0500

    adds settings.

--- settings.py
+++ settings.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import unicode_literals
-import os
+import json, os
+
 
 LOG_PATH = unicode( os.environ['ANXEOD__LOG_PATH'] )
+
+SOURCE_DIR = unicode( os.environ['ANXEOD__SOURCE_DIR_PATH'] )
+ARCHIVE_ORIGINAL_DIR = unicode( os.environ['ANXEOD__ARCHIVE_ORIGINAL_DIR_PATH'] )
+ARCHIVE_PARSED_DIR = unicode( os.environ['ANXEOD__ARCHIVE_PARSED_DIR_PATH'] )
+DESTINATION_DIR = unicode( os.environ['ANXEOD__DESTINATION_DIR_PATH'] )
+PREFIX_LIST = json.loads( os.environ['ANXEOD__PREFIX_LIST_JSON'] )
