f3bd3d66eb13fab9eaaab1bdcac2a349e48c8d02
birkin <birkin.diana@gmail.com>
2016-11-14 14:14:46 -0500

    updates dateprepper header.

--- DatePrepper.py
+++ DatePrepper.py
@@ -1,7 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+import logging, time
+
 """
 Part of LAS-to-Josiah code.
 Obtain date in right format when needed.
 """
+
+
+log = logging.getLogger(__name__)
 
 
 class DatePrepper:
@@ -13,18 +22,19 @@ class DatePrepper:
 
 
 	def obtainDate(self):
-		import time
+		# import time
 		if( len(self.timeToFormat) == 0):
 			theTime = time.localtime()
 		else:
 			theTime = self.timeToFormat
 		formattedTime = time.strftime("%a %b %d %H:%M:%S %Z %Y", theTime)
+        log.debug( 'type, formattedTime, `{}`'.format(type(formattedTime)) )
 		return formattedTime
 
 
 
 	def prepareTimeStamp(self):
-		import time
+		# import time
 		if( len(self.timeToFormat) == 0):
 			theTime = time.localtime()
 		else:
