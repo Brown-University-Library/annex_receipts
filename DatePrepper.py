7e1fd5e20924bd0608c0cf4c2161a1de900e9dfa
birkin <birkin.diana@gmail.com>
2016-11-15 09:28:43 -0500

    removes unused code.

--- DatePrepper.py
+++ DatePrepper.py
@@ -18,16 +18,6 @@ class DatePrepper(object):
     def __init__(self):
         self.timeToFormat = ""
 
-    def obtainDate(self):
-        # import time
-        if( len(self.timeToFormat) == 0):
-            theTime = time.localtime()
-        else:
-            theTime = self.timeToFormat
-        formattedTime = time.strftime("%a %b %d %H:%M:%S %Z %Y", theTime)
-        log.debug( 'type, formattedTime, `{}`'.format(type(formattedTime)) )
-        return formattedTime
-
     def prepareTimeStamp(self):
         if( len(self.timeToFormat) == 0):
             theTime = time.localtime()
