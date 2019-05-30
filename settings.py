0769f529a621f7bf48606325e3ab0c911b43c66b
birkin <birkin.diana@gmail.com>
2019-05-30 16:24:50 -0400

    removes unicode references.

--- settings.py
+++ settings.py
@@ -1,24 +1,23 @@
 # -*- coding: utf-8 -*-
 
-from __future__ import unicode_literals
 import json, os
 
 
-LOG_PATH = unicode( os.environ['ANXEOD__LOG_PATH'] )
-LOG_LEVEL = unicode( os.environ['ANXEOD__LOG_LEVEL'] )
+LOG_PATH = os.environ['ANXEOD__LOG_PATH']
+LOG_LEVEL = os.environ['ANXEOD__LOG_LEVEL']
 assert LOG_LEVEL in [ 'D', 'I', 'E' ]  # debug, info, error
 
-SOURCE_DIR = unicode( os.environ['ANXEOD__SOURCE_DIR_PATH'] )                       # should end in '/'
-ARCHIVE_ORIGINAL_DIR = unicode( os.environ['ANXEOD__ARCHIVE_ORIGINAL_DIR_PATH'] )   # should end in '/'
-ARCHIVE_PARSED_DIR = unicode( os.environ['ANXEOD__ARCHIVE_PARSED_DIR_PATH'] )       # should end in '/'
-DESTINATION_DIR = unicode( os.environ['ANXEOD__DESTINATION_DIR_PATH'] )             # should end in '/'
+SOURCE_DIR = os.environ['ANXEOD__SOURCE_DIR_PATH']                       # should end in '/'
+ARCHIVE_ORIGINAL_DIR = os.environ['ANXEOD__ARCHIVE_ORIGINAL_DIR_PATH']   # should end in '/'
+ARCHIVE_PARSED_DIR = os.environ['ANXEOD__ARCHIVE_PARSED_DIR_PATH']       # should end in '/'
+DESTINATION_DIR = os.environ['ANXEOD__DESTINATION_DIR_PATH']             # should end in '/'
 
 PREFIX_LIST = json.loads( os.environ['ANXEOD__PREFIX_LIST_JSON'] )
 
-MAIL_SMTPSERVER = unicode( os.environ['ANXEOD__MAIL_SMTPSERVER'] )
+MAIL_SMTPSERVER = os.environ['ANXEOD__MAIL_SMTPSERVER']
 MAIL_RECIPIENTS = json.loads( os.environ['ANXEOD__MAIL_RECIPIENTS_JSON'] )
-MAIL_SENDER = unicode( os.environ['ANXEOD__MAIL_SENDER'] )
-MAIL_HEADERTO = unicode( os.environ['ANXEOD__MAIL_HEADERTO'] )
-MAIL_HEADERCC = unicode( os.environ['ANXEOD__MAIL_HEADERCC'] )
-MAIL_HEADERFROM = unicode( os.environ['ANXEOD__MAIL_HEADERFROM'] )
-MAIL_SUBJECT = unicode( os.environ['ANXEOD__MAIL_SUBJECT'] )
+MAIL_SENDER = os.environ['ANXEOD__MAIL_SENDER']
+MAIL_HEADERTO = os.environ['ANXEOD__MAIL_HEADERTO']
+MAIL_HEADERCC = os.environ['ANXEOD__MAIL_HEADERCC']
+MAIL_HEADERFROM = os.environ['ANXEOD__MAIL_HEADERFROM']
+MAIL_SUBJECT = os.environ['ANXEOD__MAIL_SUBJECT']
--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from __future__ import unicode_literals
 
 import datetime, logging, os, pprint, sys, time, unittest
+
 parent_working_dir = os.path.abspath( os.path.join(os.getcwd(), os.pardir) )
 sys.path.append( parent_working_dir )
 from annex_eod_project import settings
